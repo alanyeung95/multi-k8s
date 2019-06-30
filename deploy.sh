3cd8d9b5643c4ac597ffbbeb48a44e5626eac0b7
Alan Yeung <alan.yeung@scmp.com>
2019-06-30 18:26:30 +0800

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 docker build -t alanyeung95/multi-client:latest -t alanyeung95/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t alanyeung95/multi-server:latest -t alanyeung95/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t alanyeung95/multi-worker:latest -t alanyeung95/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-dockedr push alanyeung95/multi-cilent:latest
-dockedr push alanyeung95/multi-server:latest
-dockedr push alanyeung95/multi-worker:latest
-dockedr push alanyeung95/multi-cilent:$SHA
-dockedr push alanyeung95/multi-server:$SHA
-dockedr push alanyeung95/multi-worker:$SHA
+docker push alanyeung95/multi-cilent:latest
+docker push alanyeung95/multi-server:latest
+docker push alanyeung95/multi-worker:latest
+docker push alanyeung95/multi-cilent:$SHA
+docker push alanyeung95/multi-server:$SHA
+docker push alanyeung95/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=alanyeung95/multi-server:$SHA
 kubectl set image deployments/client-deployment client=alanyeung95/multi-client:$SHA
